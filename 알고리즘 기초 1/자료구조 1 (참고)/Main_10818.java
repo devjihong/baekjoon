f273965b0e617b39f89347c0836fc29508bc35ca
devjihong <02096679@naver.com>
2022-03-07 11:25:54 +0900

    상수 네이밍 수정

--- 알고리즘 기초 1/자료구조 1 (참고)/Main_10818.java
+++ 알고리즘 기초 1/자료구조 1 (참고)/Main_10818.java
@@ -1,13 +1,13 @@
 import java.io.*;
 
 public class Main_10818 {
-    public static final int alphaSize = 26;
+    public static final int ALPHABET_SIZE = 26;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int[] result = new int[alphaSize];
+        int[] result = new int[ALPHABET_SIZE];
         
-        for (int i = 0; i < alphaSize; i++) {
+        for (int i = 0; i < ALPHABET_SIZE; i++) {
             result[i] = 0;
         }
         String st = br.readLine();
@@ -17,7 +17,7 @@ public class Main_10818 {
             result[index]++;
         }
         
-        for (int i = 0; i < alphaSize; i++) {
+        for (int i = 0; i < ALPHABET_SIZE; i++) {
             bw.write(result[i]+" ");
         }
         bw.flush();
