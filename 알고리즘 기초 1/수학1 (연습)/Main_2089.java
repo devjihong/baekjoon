ee9de3b5e369ce8972dc5b69571303946792db43
devjihong <02096679@naver.com>
2022-05-15 14:41:12 +0900

    -2진수

---
+++ 알고리즘 기초 1/수학1 (연습)/Main_2089.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main_2089 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+        int input = Integer.parseInt(br.readLine());
+
+        if (input == 0) {
+            System.out.println(0);
+        } else {
+            while (input != 1) {
+                sb.append(Math.abs(input % -2));
+                input = (int)Math.ceil((double) input / (-2));
+            }
+            sb.append(input);
+            System.out.println(sb.reverse());
+        }
+    }
+}
